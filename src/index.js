71cfc452d9be2b0a81dd0d881d91f5ffce352fee
shreshth-xd <shreshthkanchan777@gmail.com>
2026-07-17 16:23:17 +0530

    Made a check earlier to see if embeddings are working or not

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import qdrant from "./config/qdrant.js";
 import { testQdrantConnection, initializeQdrant } from "./db/initQdrant.js";
 import { seedQdrant } from "./db/seedQdrant.js";
 import { searchQdrant } from "./db/searchQdrant.js";
+import { generateEmbedding } from "./services/embeddingService.js";
 
 
 const app = express();
@@ -16,9 +17,18 @@ app.get("/", (req, res) => {
   res.send("RAG Engine is running 🚀");
 });
 
-await initializeQdrant();
-await seedQdrant();
-await searchQdrant();
+// await initializeQdrant();
+// await seedQdrant();
+// await searchQdrant();
+
+// Generating an embedding using gemini-embedding-001 for now, will move to gemini-embedding-2 real soon
+
+const embedding = await generateEmbedding(
+  "Docker is a platform for developing and running containers."
+);
+
+console.log("Embedding length:", embedding.length);
+console.log("First 10 values:", embedding.slice(0, 10));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
