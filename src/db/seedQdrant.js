9b7862661f376821e37d76fd72e5230e26015570
shreshth-xd <shreshthkanchan777@gmail.com>
2026-07-17 14:59:11 +0530

    Adding one more point

--- src/db/seedQdrant.js
+++ src/db/seedQdrant.js
@@ -1,19 +1,27 @@
 import qdrant from "../config/qdrant.js";
 
 export async function seedQdrant() {
-  await qdrant.upsert("documents", {
-    wait: true,
-    points: [
-      {
-        id: 1,
-        vector: Array.from({ length: 3072 }, () => Math.random()),
-        payload: {
-          text: "Docker is a platform for developing and running containers.",
-          source: "docker.md",
-        },
-      },
-    ],
-  });
+    await qdrant.upsert("documents", {
+        wait: true,
+        points: [
+            {
+                id: 1,
+                vector: Array.from({ length: 3072 }, () => Math.random()),
+                payload: {
+                    text: "Docker is a platform for developing and running containers.",
+                    source: "docker.md",
+                },
+            },
+            {
+                id: 2,
+                vector: Array.from({ length: 3072 }, () => Math.random()),
+                payload: {
+                    text: "Express is a minimalist web framework for Node.js.",
+                    source: "express.md",
+                },
+            },
+        ],
+    });
 
-  console.log("✅ First point inserted.");
+    console.log("✅ First point inserted.");
 }
